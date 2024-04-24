2d13389882e17c80a502b0341944ff5197b5ca61
김라연 <fkdusrh@naver.com>
2024-04-24 16:37:55 +0900

    [#7] feat: 게임 관련 Entity 작성

--- backend/game/src/main/java/com/wak/game/domain/player/Player.java
+++ backend/game/src/main/java/com/wak/game/domain/player/Player.java
@@ -1,16 +1,70 @@
 package com.wak.game.domain.player;
 
+import com.wak.game.domain.round.Round;
+import com.wak.game.domain.user.User;
 import com.wak.game.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "players")
 public class Player extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "room_player_id")
+    @Column(name = "player_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "round_id", nullable = false)
+    private Round round;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "murder_player_id")
+    private Player murderPlayer;
+
+    @Column(name = "stamina")
+    private Integer stamina;
+
+    @Column(name = "alive_time")
+    private String aliveTime;
+
+    @Column(name = "kill_cnt")
+    private Integer killCount;
+
+    @Column(name = "rank")
+    private Integer rank;
+
+    @Column(name = "is_player")
+    private Boolean isPlayer;
+
+    @Column(name = "team")
+    private Integer team;
+
+    @Column(name = "is_queen")
+    private Boolean isQueen;
+
+    @Builder
+    public Player(Long id, User user, Round round, Player murderPlayer, Integer stamina, String aliveTime,
+                  Integer killCount, Integer rank, Boolean isPlayer, Integer team, Boolean isQueen) {
+        this.id = id;
+        this.user = user;
+        this.round = round;
+        this.murderPlayer = murderPlayer;
+        this.stamina = stamina;
+        this.aliveTime = aliveTime;
+        this.killCount = killCount;
+        this.rank = rank;
+        this.isPlayer = isPlayer;
+        this.team = team;
+        this.isQueen = isQueen;
+    }
 }
