dbe470f7a26c8e1133c720318aea51b8d5277a82
siyoon <ryuu.public@gmail.com>
2024-04-25 15:01:27 +0900

    [#3] feat: add CORS configuration class and set up the values

---
+++ backend/game/src/main/java/com/wak/game/global/config/web/WebConfig.java
@@ -0,0 +1,29 @@
+package com.wak.game.global.config.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${origin.frontend}")
+    private String frontendOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontendOrigin)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                );
+    }
+
+}
