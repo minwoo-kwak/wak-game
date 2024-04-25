f1120756905ca704a6377726b557aa30a9d9df9b
siyoon <ryuu.public@gmail.com>
2024-04-25 16:54:27 +0900

    [#4] fix: test utility

--- backend/game/src/test/java/com/wak/game/global/util/RequestUtil.java
+++ backend/game/src/test/java/com/wak/game/global/util/RequestUtil.java
@@ -3,9 +3,13 @@ package com.wak.game.global.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
@@ -33,6 +37,11 @@ public class RequestUtil {
         return null;
     }
 
+    public static class UrlTemplate {
+
+
+    }
+
     public MockHttpServletRequestBuilder getRequest(String urlTemplate, Object... uriVariables) {
         return get(urlTemplate, uriVariables);
     }
@@ -49,24 +58,42 @@ public class RequestUtil {
         return delete(urlTemplate, uriVariables);
     }
 
-    public <T> ResultMatcher jsonContent(Class<T> clazz, T object) throws JsonProcessingException {
-        checkClass(clazz, object);
-        ApiResult<T> expectedResponse = ApiUtils.success(object);
-        String expectedJsonResponse = objectMapper.writeValueAsString(expectedResponse);
+    public MockHttpServletRequestBuilder getRequestWithJson(String urlTemplate, Object request, Object... uriVariables) throws JsonProcessingException {
+        return getRequest(urlTemplate, uriVariables)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request));
+    }
+
+    public MockHttpServletRequestBuilder postRequestWithJson(String urlTemplate, Object request, Object... uriVariables) throws JsonProcessingException {
+        return postRequest(urlTemplate, uriVariables)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request));
+    }
+
+    public MockHttpServletRequestBuilder putRequestWithJson(String urlTemplate, Object request, Object... uriVariables) throws JsonProcessingException {
+        return putRequest(urlTemplate, uriVariables)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request));
+    }
+
+    public MockHttpServletRequestBuilder deleteRequestWithJson(String urlTemplate, Object request, Object... uriVariables) throws JsonProcessingException {
+        return deleteRequest(urlTemplate, uriVariables)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request));
+    }
+
+    public <T> ResultMatcher jsonContent(T object) throws JsonProcessingException {
+        String expectedJsonResponse = objectMapper.writeValueAsString(ApiUtils.success(object));
         return content().json(expectedJsonResponse);
     }
 
-    public <T> ResultMatcher jsonListContent(Class<T> clazz, List<T> object) throws JsonProcessingException {
-        checkClass(clazz, object.get(0));
-        ApiResult<List<T>> expectedResponse = ApiUtils.success(object);
-        String expectedJsonResponse = objectMapper.writeValueAsString(expectedResponse);
+    public <T> ResultMatcher jsonListContent(List<T> object) throws JsonProcessingException {
+        String expectedJsonResponse = objectMapper.writeValueAsString(ApiUtils.success(object));
         return content().json(expectedJsonResponse);
     }
 
-    private <T> void checkClass(Class<T> clazz, T object) {
-        if (!clazz.isInstance(object)) {
-            throw new IllegalArgumentException("Argument must be an instance of " + clazz.getSimpleName());
-        }
+    public StatusResultMatchers status() {
+        return MockMvcResultMatchers.status();
     }
 
 }
