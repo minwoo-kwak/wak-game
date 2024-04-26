93aec06e253c4a1deb4afa8f388f98b3343d335c
slcloe <67732600+slcloe@users.noreply.github.com>
2024-04-26 14:52:02 +0900

    [#10] feat: color 초기화
    앞으로 color 초기화 할 때 resources/asset/color.json 에서 체크하면 됨.

--- backend/game/src/main/java/com/wak/game/domain/color/ColorRepositoryCustom.java
+++ backend/game/src/main/java/com/wak/game/domain/color/ColorRepositoryCustom.java
@@ -1,5 +1,7 @@
 package com.wak.game.domain.color;
 
+import java.util.Optional;
+
 public interface ColorRepositoryCustom {
 
     Color findByHexColor(String hexColor);
--- backend/game/src/main/java/com/wak/game/domain/color/ColorService.java
+++ backend/game/src/main/java/com/wak/game/domain/color/ColorService.java
@@ -1,8 +1,30 @@
 package com.wak.game.domain.color;
 
+import com.wak.game.global.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ColorService {
+
+    private final ColorRepository colorRepository;
+
+    public Color findById(long id){
+        return colorRepository.findById(id).orElseThrow(); // ErrorCode
+    }
+
+    public Color save(String hexColor) {
+        if (!validCheckDuplicate(hexColor)) // ErrorCode
+            return null;
+
+        return colorRepository.save(Color.builder()
+                .hexColor(hexColor)
+                .build());
+    }
+
+    private boolean validCheckDuplicate(String hexColor){
+        if (colorRepository.findByHexColor(hexColor) == null)
+            return true;
+        else return false;
+    }
 }
