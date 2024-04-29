46ba40e2020cbc0cc51a9dfc7a9180f51fae021d
slcloe <67732600+slcloe@users.noreply.github.com>
2024-04-29 16:22:35 +0900

    [#15] feat: login & user info 조회 Controller update

--- backend/game/src/main/java/com/wak/game/application/controller/UserController.java
+++ backend/game/src/main/java/com/wak/game/application/controller/UserController.java
@@ -1,11 +1,34 @@
 package com.wak.game.application.controller;
 
+import com.wak.game.application.facade.UserFacade;
+import com.wak.game.application.request.UserLogInRequest;
+import com.wak.game.application.response.UserInfoResponse;
+import com.wak.game.application.response.UserLogInResponse;
+import com.wak.game.global.token.AuthUser;
+import com.wak.game.global.util.ApiUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
-@RequestMapping("/user")
+@RequestMapping("api/users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserFacade userFacade;
+
+    @PostMapping("")
+    public ResponseEntity<?> login(@RequestBody UserLogInRequest request) {
+        UserLogInResponse response = userFacade.login(request);
+        return ResponseEntity.ok(ApiUtils.success(response));
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<?> getUserInfo(@AuthUser Long id){
+        UserInfoResponse response = userFacade.userInfo(id);
+        return ResponseEntity.ok(ApiUtils.success(response));
+    }
+
 }
