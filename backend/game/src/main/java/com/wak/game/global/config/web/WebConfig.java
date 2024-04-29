f1918b5ca9736c60134f0e6e4dc818b6f7756c5e
slcloe <67732600+slcloe@users.noreply.github.com>
2024-04-29 16:22:12 +0900

    [#15] feat: JWT Token update

--- backend/game/src/main/java/com/wak/game/global/config/web/WebConfig.java
+++ backend/game/src/main/java/com/wak/game/global/config/web/WebConfig.java
@@ -1,16 +1,39 @@
 package com.wak.game.global.config.web;
 
+import com.wak.game.global.token.AuthUserArgumentResolver;
+import com.wak.game.global.token.JWTInterceptor;
+import com.wak.game.global.token.JWTUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${origin.frontend}")
     private String frontendOrigin;
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+    private final JWTInterceptor jwtInterceptor;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/users");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
