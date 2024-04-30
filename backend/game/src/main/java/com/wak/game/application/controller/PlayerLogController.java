24f0d20ca4ffe941d9a5698098611953003e4034
slcloe <67732600+slcloe@users.noreply.github.com>
2024-04-30 15:10:39 +0900

    [#20] feat : rest api uri 수정 "/api/" 추가

--- backend/game/src/main/java/com/wak/game/application/controller/PlayerLogController.java
+++ backend/game/src/main/java/com/wak/game/application/controller/PlayerLogController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/player-log")
+@RequestMapping("/api/player-log")
 @RequiredArgsConstructor
 public class PlayerLogController {
 }
--- backend/game/src/main/java/com/wak/game/application/controller/UserController.java
+++ backend/game/src/main/java/com/wak/game/application/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
