67dfc1cc2b41d7bc309324627306d0f6222aa532
김이현 <rihyun1126@naver.com>
2024-05-02 11:05:55 +0900

    [#22] fix: change allowed origin patterns

--- backend/game/src/main/java/com/wak/game/global/config/web/WebConfig.java
+++ backend/game/src/main/java/com/wak/game/global/config/web/WebConfig.java
@@ -38,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", frontendOrigin)
+                .allowedOriginPatterns("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
