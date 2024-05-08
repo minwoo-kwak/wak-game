7ee74a324c56fb836f06800c91adc96b17d97c73
slcloe <67732600+slcloe@users.noreply.github.com>
2024-05-08 15:27:12 +0900

    [#28] feat: enterRoom 게임 시작 여부 유효성 검사 추가 및 ResponseDTO 수정

--- backend/game/src/main/java/com/wak/game/application/response/RoomBasicInfoResponse.java
+++ backend/game/src/main/java/com/wak/game/application/response/RoomBasicInfoResponse.java
@@ -1,16 +1,18 @@
 package com.wak.game.application.response;
 
-public record RoomBasicInfoResponse(long user_id, boolean isChief, long room_id, String room_name, String mode, int limit_players){
+public record RoomBasicInfoResponse(long user_id, boolean isChief, long room_id, String room_name, String mode, int limit_players, boolean lock){
 
-    public static RoomBasicInfoResponse of (long user_id, boolean isChief, long room_id, String room_name, String mode, int limit_players) {
-        return new RoomBasicInfoResponse(user_id, isChief, room_id, room_name, mode, limit_players);
+    public static RoomBasicInfoResponse of (long user_id, boolean isChief, long room_id, String room_name, String mode, int limit_players, boolean lock) {
+        return new RoomBasicInfoResponse(user_id, isChief, room_id, room_name, mode, limit_players, lock);
     }
-    public RoomBasicInfoResponse(long user_id, boolean isChief, long room_id, String room_name, String mode, int limit_players) {
+
+    public RoomBasicInfoResponse(long user_id, boolean isChief, long room_id, String room_name, String mode, int limit_players, boolean lock) {
         this.user_id = user_id;
         this.isChief = isChief;
         this.room_id = room_id;
         this.room_name = room_name;
         this.mode = mode;
         this.limit_players = limit_players;
+        this.lock = lock;
     }
 }
