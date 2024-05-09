fb6b4b0e58edae0e67ed7114c59a7d2791004b68
siyoon <ryuu.public@gmail.com>
2024-05-09 15:08:34 +0900

    [#29] feat: MongoDB 연결, 테스트 코드 작성

---
+++ backend/chat/src/main/java/com/wak/chat/global/config/MongoConfig.java
@@ -0,0 +1,36 @@
+package com.wak.chat.global.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+class MongoConfig {
+
+    @Value("${spring.data.mongodb.username}")
+    String username;
+
+    @Value("${spring.data.mongodb.password}")
+    String password;
+
+    @Value("${spring.data.mongodb.host}")
+    String host;
+
+    @Value("${spring.data.mongodb.port}")
+    String port;
+
+    @Bean
+    MongoClient mongoClient() {
+        String uri = String.format("mongodb://%s:%s@%s:%s", username, password, host, port);
+        return MongoClients.create(uri);
+    }
+
+    @Bean
+    MongoOperations mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "test");
+    }
+}
