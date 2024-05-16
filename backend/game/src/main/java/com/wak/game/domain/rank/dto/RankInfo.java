f46c19eca261267117f837453047607a5640b630
김라연 <fkdusrh@naver.com>
2024-05-17 01:09:51 +0900

    [#14] fix: 직렬화 위한 기본생성자 추가

--- backend/game/src/main/java/com/wak/game/domain/rank/dto/RankInfo.java
+++ backend/game/src/main/java/com/wak/game/domain/rank/dto/RankInfo.java
@@ -2,10 +2,12 @@ package com.wak.game.domain.rank.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
+@NoArgsConstructor
 public class RankInfo {
     private Long userId;
     private String nickName;
