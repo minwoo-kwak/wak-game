d848ecb7abb5e517d2202e8beb57019b623f3e50
김라연 <fkdusrh@naver.com>
2024-05-18 15:04:40 +0900

    [#14] fix: 클릭 요청시 데이터 추가

--- backend/game/src/main/java/com/wak/game/application/controller/GameController.java
+++ backend/game/src/main/java/com/wak/game/application/controller/GameController.java
@@ -3,29 +3,18 @@ package com.wak.game.application.controller;
 import com.wak.game.application.facade.PlayerFacade;
 import com.wak.game.application.facade.RoundFacade;
 import com.wak.game.application.request.ClickRequest;
-import com.wak.game.global.token.AuthUser;
-import com.wak.game.global.util.ApiResult;
-import com.wak.game.global.util.ApiUtils;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class GameController {
     private final PlayerFacade playerFacade;
-    private final RoundFacade roundFacade;
 
     @MessageMapping("/click/{roomId}")
-    public void handleClick(@PathVariable Long roomId, ClickRequest clickRequest) {
-        playerFacade.saveClickLog(roomId,clickRequest);
+    public void handleClick(@RequestBody ClickRequest clickRequest) {
+        playerFacade.saveClickLog( clickRequest);
     }
 }
