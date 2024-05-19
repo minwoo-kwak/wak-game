c897a3b5b686310fcb2ac0efd3b0a7de76e98671
김라연 <fkdusrh@naver.com>
2024-05-19 21:19:06 +0900

    [#14] fix: aggro

--- backend/game/src/main/java/com/wak/game/application/request/socket/MentionRequest.java
+++ backend/game/src/main/java/com/wak/game/application/request/socket/MentionRequest.java
@@ -1,6 +1,6 @@
 package com.wak.game.application.request.socket;
 
-public record MentionRequest(Long userId, Long roomId, Long roundId, String mention) {
+public record MentionRequest(Long userId, Long roomId, Long roundId, Long nextRoundId, String mention) {
     public MentionRequest {
     }
 }
